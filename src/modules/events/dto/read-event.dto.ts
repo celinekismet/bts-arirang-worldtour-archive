5f7b4cd76dc862c8025dc107145bd52a4a7163a3
celine <celine.kismet@gmail.com>
2026-09-05 20:07:06 +0200

    feature: add dtos for event modules

--- src/modules/events/dto/read-event.dto.ts
+++ src/modules/events/dto/read-event.dto.ts
@@ -1,10 +1,62 @@
-import { HitTweet } from "../../hit-tweets/entities/hit-tweet.entity.js";
-import { Media } from "../../media/entities/media.entity.js";
-import { Outfit } from "../../outfits/entities/outfit.entity.js";
-import { PrimaryGeneratedColumn } from "typeorm";
-import { IsArray, IsString } from "class-validator";
+
 
 export class ReadEventDto {
 
+  /**
+   * The unique identifier for the event.
+   */
+  eventId: number;
 
+  /**
+   * The date of the event.
+   */
+  date: Date;
+
+  /**
+   * The setlist for the event.
+   */
+  setlist: string[];
+
+  /**
+   * The number of tickets sold for the event.
+   */
+  ticketsSold: number;
+
+  /**
+   * The attendance for the event.
+   */
+  attendance: number;
+
+  /**
+   * The city and country of the event.
+   */
+  location: {
+    city: string;
+    country: string;
+  };
+
+  /**
+   * The outfits associated with the event.
+   */
+  outfits: { outfitId: number; name: string }[];
+
+  /**
+   * The surprise songs performed at the event.
+   */
+  surpriseSongs: { surpriseSongId: number; title: string }[];
+
+  /**
+   * The media (photos/videos) associated with the event.
+   */
+  media: { mediaId: number; url: string; type: string }[];
+
+  /**
+   * The highlights of the event.
+   */
+  highlights: { highlightId: number; description: string }[];
+
+  /**
+   * The hit tweets associated with the event.
+   */
+  tweets: { hitTweetId: number; tweetUrl: string }[];
 }
