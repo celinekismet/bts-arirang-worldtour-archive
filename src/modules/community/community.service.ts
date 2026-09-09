a021731e377f767da98a7b4407bd179531137ab3
celine <celine.kismet@gmail.com>
2026-09-09 22:57:50 +0200

    feature: community service last method finalization

--- src/modules/community/community.service.ts
+++ src/modules/community/community.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommunityDto } from './dto/create-community.dto.js';
 import { UpdateCommunityDto } from './dto/update-community.dto.js';
-import { JwtAuthGuard } from '../../auth/jwt-auth.guard.js';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Community } from './entities/community.entity.js';
 import { Repository } from 'typeorm';
@@ -20,17 +19,19 @@ export class CommunityService {
   }
 
   findAll() {
-    return `This action returns all community`;
+    return this.communityRepository.find();
   }
-  findOne(id: number) {
-    return `This action returns a #${id} community`;
-  }
-
-  update(id: number, updateCommunityDto: UpdateCommunityDto) {
-    return `This action updates a #${id} community`;
+  
+  findOne(id: number): Promise<Community | null> {
+    return this.communityRepository.findOneBy({ communityId: id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} community`;
+  async update(id: number, updateCommunityDto: UpdateCommunityDto): Promise<Community | null> {
+    await this.communityRepository.update( id, updateCommunityDto)
+      return this.findOne(id);
+  }
+
+  async remove(id: number):Promise<void> {
+    await this.communityRepository.delete(id);
   }
 }
