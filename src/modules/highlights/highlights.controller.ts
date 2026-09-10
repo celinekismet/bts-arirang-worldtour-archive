b93c6903f7704bb6484377d3b7e705d80e78a71c
celine <celine.kismet@gmail.com>
2026-09-10 23:56:37 +0200

    feature: highlight service methods create, findall and findone

--- src/modules/highlights/highlights.controller.ts
+++ src/modules/highlights/highlights.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { HighlightsService } from './highlights.service.js';
 import { CreateHighlightDto } from './dto/create-highlight.dto.js';
 import { UpdateHighlightDto } from './dto/update-highlight.dto.js';
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard.js';
 
 @Controller('highlights')
 export class HighlightsController {
   constructor(private readonly highlightsService: HighlightsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createHighlightDto: CreateHighlightDto) {
     return this.highlightsService.create(createHighlightDto);
@@ -22,11 +24,13 @@ export class HighlightsController {
     return this.highlightsService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateHighlightDto: UpdateHighlightDto) {
     return this.highlightsService.update(+id, updateHighlightDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.highlightsService.remove(+id);
--- src/modules/highlights/highlights.service.ts
+++ src/modules/highlights/highlights.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateHighlightDto } from './dto/create-highlight.dto.js';
 import { UpdateHighlightDto } from './dto/update-highlight.dto.js';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Highlight } from './entities/highlight.entity.js';
 
 @Injectable()
 export class HighlightsService {
+
+  constructor(
+    @InjectRepository(Highlight)
+    private readonly highlightsRepository: Repository<Highlight>
+   ){}
   create(createHighlightDto: CreateHighlightDto) {
     return 'This action adds a new highlight';
   }
 
-  findAll() {
-    return `This action returns all highlights`;
+  findAll():Promise<Highlight[]> {
+    return this.highlightsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} highlight`;
+  findOne(id: number): Promise<Highlight | null> {
+    return this.highlightsRepository.findOne({
+      where: { highlightId: id},
+      relations: {
+      media: true,
+      event: true,
+      tweets: true,
+      members: true,
+    },
+    })
   }
 
   update(id: number, updateHighlightDto: UpdateHighlightDto) {
