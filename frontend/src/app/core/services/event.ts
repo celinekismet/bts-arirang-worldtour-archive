79f7460fd1f4c42eedce8cb859e1637835f09a01
celine <celine.kismet@gmail.com>
2026-09-24 23:43:21 +0200

    feat: add service for event

--- frontend/src/app/core/services/event.ts
+++ frontend/src/app/core/services/event.ts
@@ -1,6 +1,21 @@
-import { Service } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
-@Service()
+@Injectable({
+  providedIn: 'root',
+})
 export class EventService {
+    private readonly apiUrl = `${ environment.apiUrl }/event`
 
+    constructor(private http: HttpClient ){}
+
+    getAll(): Observable<Event[]>{
+        return this.http.get<Event[]>(this.apiUrl)
+    }
+
+    getOne(id: number): Observable<Event> {
+        return this.http.get<Event>(`${ this.apiUrl}/${id}`)
+    }
 }
